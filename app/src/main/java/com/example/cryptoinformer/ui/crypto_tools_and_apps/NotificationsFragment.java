c138f2d703e3af5a37fc74cef95795dcf602f732
Maguire, Eamon <maguire_eamon@yahoo.com>
2020-02-06 22:06:30 -0800

    initial prototype commit

--- app/src/main/java/com/example/cryptoinformer/ui/crypto_tools_and_apps/NotificationsFragment.java
+++ app/src/main/java/com/example/cryptoinformer/ui/crypto_tools_and_apps/NotificationsFragment.java
@@ -1,4 +1,4 @@
-package com.example.cryptoinformer.ui.notifications;
+package com.example.cryptoinformer.ui.crypto_tools_and_apps;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -22,7 +22,7 @@ public class NotificationsFragment extends Fragment {
                              ViewGroup container, Bundle savedInstanceState) {
         notificationsViewModel =
                 ViewModelProviders.of(this).get(NotificationsViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_notifications, container, false);
+        View root = inflater.inflate(R.layout.fragment_tools_and_apps, container, false);
         final TextView textView = root.findViewById(R.id.text_notifications);
         notificationsViewModel.getText().observe(this, new Observer<String>() {
             @Override
--- app/src/main/java/com/example/cryptoinformer/ui/crypto_tools_and_apps/NotificationsViewModel.java
+++ app/src/main/java/com/example/cryptoinformer/ui/crypto_tools_and_apps/NotificationsViewModel.java
@@ -1,16 +1,16 @@
-package com.example.cryptoinformer.ui.dashboard;
+package com.example.cryptoinformer.ui.crypto_tools_and_apps;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class DashboardViewModel extends ViewModel {
+public class NotificationsViewModel extends ViewModel {
 
     private MutableLiveData<String> mText;
 
-    public DashboardViewModel() {
+    public NotificationsViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is dashboard fragment");
+        mText.setValue("This is notifications fragment");
     }
 
     public LiveData<String> getText() {
