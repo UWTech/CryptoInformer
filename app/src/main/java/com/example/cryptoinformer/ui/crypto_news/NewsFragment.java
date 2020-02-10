14e93b85e5a4a7680ddc4fcbca00fa33d620a10f
Maguire, Eamon <maguire_eamon@yahoo.com>
2020-02-09 18:02:19 -0800

    add rss feed
    added RSS parsing module
    rudimentary dynamic RSS view

--- app/src/main/java/com/example/cryptoinformer/ui/crypto_news/NewsFragment.java
+++ app/src/main/java/com/example/cryptoinformer/ui/crypto_news/NewsFragment.java
@@ -1,9 +1,11 @@
 package com.example.cryptoinformer.ui.crypto_news;
 
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -13,6 +15,11 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.example.cryptoinformer.R;
+import com.example.cryptoinformer.ui.crypto_news.cryptoRss.RSSRetriver;
+
+import org.xmlpull.v1.XmlPullParserException;
+
+import java.io.IOException;
 
 public class NewsFragment extends Fragment {
 
@@ -20,9 +27,34 @@ public class NewsFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
+
+        RSSRetriver rssRetriver = new RSSRetriver();
+
         homeViewModel =
                 ViewModelProviders.of(this).get(NewsViewModel.class);
         View root = inflater.inflate(R.layout.fragment_news, container, false);
+
+        LinearLayout newsLinearLayout = (LinearLayout) root.findViewById(R.id.news_linear_layout);
+
+        //TODO:: remove this after making class async
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+
+        StrictMode.setThreadPolicy(policy);
+
+        try {
+            // populate RSS feed info in class instance
+            rssRetriver.getRSSFeed();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        // dynamically add RSS feed items to view
+        for (String title: rssRetriver.getTitles()) {
+            TextView dynamicNewsViewElement = new TextView(getContext());
+            dynamicNewsViewElement.setText(title);
+            newsLinearLayout.addView(dynamicNewsViewElement);
+        }
+
         final TextView textView = root.findViewById(R.id.text_home);
         homeViewModel.getText().observe(this, new Observer<String>() {
             @Override
